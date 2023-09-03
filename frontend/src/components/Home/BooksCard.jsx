00eac6ab0dc1ca7ee88a62a6004c93fe28f48520
Imran <imranmansuri038@gmail.com>
2023-09-03 11:13:01 +0530

    commit init

---
+++ frontend/src/components/Home/BooksCard.jsx
@@ -0,0 +1,15 @@
+import BookSingleCard from "./BookSingleCard";
+
+// eslint-disable-next-line react/prop-types
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {/* eslint-disable-next-line react/prop-types */}
+      {books.map((item) => (
+        <BookSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
